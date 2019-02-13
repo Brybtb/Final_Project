b1accea619f3bfd14716fcb9bb1f246d72a9ab28
Cassie <cnellis@alumni.unc.edu>
2019-02-12 20:31:08 -0500

    updated flask progress

--- flask_progress/app.py
+++ flask_progress/app.py
@@ -4,8 +4,8 @@ from sklearn.externals import joblib
 # import pandas as pd
 # import numpy as np
 
-sds = joblib.load("Scaler.model")
-clf = joblib.load("RandomForestFinal.model")
+sds = joblib.load("scaler.model2")
+clf = joblib.load("RandomForestFinal.model1")
 
 app = Flask(__name__)
 
@@ -26,15 +26,15 @@ def data():
 def predict():
     
     # 'AvgGlucose', 'BMI', 'Age', 'EverMarried_Yes', 'ResidenceType_Urban', 'Gender_Male'
-    avgGlucose = request.args.get("avgGlucose") or 0
+    AvgGlucose = request.args.get("AvgGlucose") or 0
     BMI = request.args.get("BMI") or 0
-    age = request.args.get("age") or 0
-    marriage = request.args.get("marriage") or 0
-    gender = request.args.get("gender") or 0
-    residence = request.args.get("residence") or 0
+    Age = request.args.get("Age") or 0
+    EverMarried_Yes = request.args.get("EverMarried_Yes") or 0
+    ResidenceType_Urban = request.args.get('ResidenceType_Urban')
+    Gender_Male = request.args.get("Gender_Male")
 
-    print("#"*80)
-    features = [[avgGlucose, BMI, age, gender, residence, marriage]]
+    #print("#"*80)
+    features = [[AvgGlucose, BMI, Age, EverMarried_Yes, ResidenceType_Urban, Gender_Male]]
     print(features)
 
     scaled_features = sds.transform(features)
@@ -45,7 +45,22 @@ def predict():
 
     print(spred)
     print(spred_prob)
-    print("#"*80)
+    #print("#"*80)
+
+    #return render_template('result.html', form=form)
+    
+# @app.route('/result',methods = ['POST'])
+# def result():
+#     if request.method == 'POST':
+#         to_predict_list = request.form.to_dict()
+#         to_predict_list=list(to_predict_list.values())
+#         to_predict_list = list(map(int, to_predict_list))
+#         result = ValuePredictor(to_predict_list)
+#         if int(result)==1:
+#             prediction='Income more than 50K'
+#         else:
+#             prediction='Income less that 50K'
+#         return render_template("result.html",prediction=prediction)
 
     return jsonify([{"survival_pred": spred.tolist(), 
                      "survival_probs": spred_prob.tolist(),
