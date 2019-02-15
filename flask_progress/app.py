227ffe91a97f3b15f27a19f1185364bec34e8d32
Cassie <cnellis@alumni.unc.edu>
2019-02-14 19:24:23 -0500

    render results on same page

--- flask_progress/app.py
+++ flask_progress/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, jsonify, render_template
 from sklearn.externals import joblib
+from flask_cors import CORS
 # import os
 # import pandas as pd
 # import numpy as np
 
-sds = joblib.load("scaler.model2")
-clf = joblib.load("RandomForestFinal.model1")
+scaler = joblib.load("scaler.model2")
+randomforest = joblib.load("RandomForestFinal.model1")
 
 app = Flask(__name__)
+CORS(app)
 
 # graphData = pd.read_csv("CleanedDataForModels.csv")
 
@@ -18,54 +20,39 @@ def index():
 @app.route('/overview')
 def overview():
     return render_template('overview.html')  
-def data():
-    graphData = graphData.to_dict(orient="record")
-    return jsonify(graphData)
+# def data():
+#     graphData = graphData.to_dict(orient="record")
+#     return jsonify(graphData)
 
 @app.route('/api/predict')
 def predict():
     
-    # 'AvgGlucose', 'BMI', 'Age', 'EverMarried_Yes', 'ResidenceType_Urban', 'Gender_Male'
     AvgGlucose = request.args.get("AvgGlucose") or 0
     BMI = request.args.get("BMI") or 0
     Age = request.args.get("Age") or 0
     EverMarried_Yes = request.args.get("EverMarried_Yes") or 0
     ResidenceType_Urban = request.args.get('ResidenceType_Urban') or 0
     Gender_Male = request.args.get("Gender_Male") or 0
-
-    #print("#"*80)
     features = [[AvgGlucose, BMI, Age, EverMarried_Yes, ResidenceType_Urban, Gender_Male]]
     print(features)
 
-    scaled_features = sds.transform(features)
+    scaled_features = scaler.transform(features)
     print(scaled_features)
 
-    spred = clf.predict(scaled_features)
-    spred_prob = clf.predict_proba(scaled_features)
+    diagnosis_pred = randomforest.predict(scaled_features)
+    diagnosis_pred_prob = randomforest.predict_proba(scaled_features)
+    print(diagnosis_pred)
+    print(diagnosis_pred_prob)
+ 
+    if diagnosis_pred == 0:diagnosisText = 'Cardiovascular Disease is Not Present'
+    else:diagnosisText = 'Cardiovascular Disease is Present'
+    print(diagnosisText)
 
-    print(spred)
-    print(spred_prob)
-    #print("#"*80)
-
-    #return render_template('result.html', form=form)
-    
-# @app.route('/result',methods = ['POST'])
-# def result():
-#     if request.method == 'POST':
-#         to_predict_list = request.form.to_dict()
-#         to_predict_list=list(to_predict_list.values())
-#         to_predict_list = list(map(int, to_predict_list))
-#         result = ValuePredictor(to_predict_list)
-#         if int(result)==1:
-#             prediction='Income more than 50K'
-#         else:
-#             prediction='Income less that 50K'
-#         return render_template("result.html",prediction=prediction)
-
-    return jsonify([{"survival_pred": spred.tolist(), 
-                     "survival_probs": spred_prob.tolist(),
+    return jsonify([{"diagnosisText": diagnosisText, 
+                     "diagnisis_pred": diagnosis_pred.tolist(), 
+                     "diagnosis_probs": diagnosis_pred_prob.tolist(),
                      "features": features,
                      "scaled_features": scaled_features.tolist()}])
-
+   
 if __name__ == "__main__":
     app.run(debug=True)
