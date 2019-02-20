9c28151a56c86ed1083756eeedb6f995f2680c91
Cassie <cnellis@alumni.unc.edu>
2019-02-19 19:41:25 -0500

    Add files via upload

--- flask_progress/app.py
+++ flask_progress/app.py
@@ -3,7 +3,7 @@ from sklearn.externals import joblib
 from flask_cors import CORS
 # import os
 # import pandas as pd
-import numpy as np
+# import numpy as np
 
 scaler = joblib.load("scaler.model2")
 randomforest = joblib.load("RandomForestFinal.model1")
@@ -25,16 +25,16 @@ def overview():
 #     return jsonify(graphData)
 
 @app.route('/api/predict')
-
 def predict():
     
-    AvgGlucose = request.args.get("avgGlucose") or 0
+    AvgGlucose1 = request.args.get("avgGlucose") or 0
+    print('this is the avg glucose:' + str(AvgGlucose1))
     BMI = request.args.get("BMI") or 0
     Age = request.args.get("Age") or 0
     EverMarried_Yes = request.args.get("EverMarried_Yes") or 0
     ResidenceType_Urban = request.args.get('ResidenceType_Urban') or 0
     Gender_Male = request.args.get("Gender_Male") or 0
-    features = [[AvgGlucose, BMI, Age, EverMarried_Yes, ResidenceType_Urban, Gender_Male]]
+    features = [[AvgGlucose1, BMI, Age, EverMarried_Yes, ResidenceType_Urban, Gender_Male]]
     print(features)
 
     scaled_features = scaler.transform(features)
@@ -42,22 +42,25 @@ def predict():
 
     diagnosis_pred = randomforest.predict(scaled_features)
     diagnosis_pred_prob = randomforest.predict_proba(scaled_features)
+    probabilityDiseasePresent = randomforest.predict_proba(scaled_features)[:,1]
     print(diagnosis_pred)
     print(diagnosis_pred_prob)
-    print(type(diagnosis_pred_prob))
-
-     
+    print(probabilityDiseasePresent)
+    
     if any((diagnosis_pred_prob[0]) < 0.15):diagnosisText = 'Not At-Risk of Cardiovascular Disease'
-    elif any((diagnosis_pred_prob[0]) < 0.23):diagnosisText = 'Slight Risk of Cardiovascular Disease is Present'
+    elif any((diagnosis_pred_prob[0]) < 0.225):diagnosisText = 'Slight Risk of Cardiovascular Disease is Present'
     elif any((diagnosis_pred_prob[0]) < 0.3):diagnosisText = 'Moderate Risk of Cardiovascular Disease is Present'
     else:diagnosisText = 'Significant Risk of Cardiovascular Disease is Present'
     print(diagnosisText)
 
-    return jsonify([{"diagnosisText": diagnosisText, 
+    return jsonify([{
+                     "features": features,
+                     "scaled_features": scaled_features.tolist(),
                      "diagnisis_pred": diagnosis_pred.tolist(), 
                      "diagnosis_probs": diagnosis_pred_prob.tolist(),
-                     "features": features,
-                     "scaled_features": scaled_features.tolist()}])
+                     "diagnosisText": diagnosisText, 
+                     "probabilityDiseasePresent": probabilityDiseasePresent.tolist()
+                     }])
    
 if __name__ == "__main__":
     app.run(debug=True)
